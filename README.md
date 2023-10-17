251e493ad881c6e091cc6e44630972cbaa2120d1
Kavita Gupta <124678732+kika8919@users.noreply.github.com>
2023-10-17 22:45:42 +0530

    adds api, error handling, promise all, nodemailer

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # email-verification-api
+
 An email verification api for LUCID GROWTH.
+
+## POST /api/verify-email HTTP/1.1
+
+Host: localhost:4000
+Content-Type: application/json
+
+### Request Body :
+
+```
+    {
+        "emails": [
+            "kavitagupta.gupta123@gmail.com",
+            "apple@gmail.com",
+            "sagar@gmail.com"
+        ]
+    }
+```
+
+### Response :
+
+```
+
+    {
+        "emails": [
+            {
+                "email": "kavitagupta.gupta123@gmail.com",
+                "isValid": true,
+                "isCatchAllEmail": false
+            },
+            {
+                "email": "apple@gmail.com",
+                "isValid": true,
+                "isCatchAllEmail": false
+            },
+            {
+                "email": "sagar@gmail.com",
+                "isValid": true,
+                "isCatchAllEmail": false
+            }
+        ]
+    }
+```
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,43 +1,87 @@
 "use strict";
 const express = require("express");
 const router = express.Router();
+const { sendEmail } = require("./sendEmail");
 
-router.get("/verify-email", (req, res, next) => {
+router.post("/verify-email", async (req, res, next) => {
   let emails = req.body.emails;
-  emails = [
-    "valid@email.com",
-    "example@email.org",
-    "random.email@12345",
-    "invalid_email@.com",
-    "name@gmail.com",
-    "john.doe@catchall.com",
-    "no_at_symbol.com",
-    "email@valid.com",
-    "random.email123@invalid.",
-    "test@example.net",
-    "user@catchall.org",
-    "invalid_email@.org",
-    "12345@email.com",
-    "catchall@randommail.xyz",
-    "name@email.co.uk",
-    "example@email",
-    "invalid@.net",
-    "valid.email@emailprovider.com",
-    "user@catchallmail.com",
-    "random@12345.email",
-  ];
+  // emails = [
+  //   // "valid@email.com",
+  //   // "example@email.org",
+  //   // "random.email@12345",
+  //   // "invalid_email@.com",
+  //   // "name@gmail.com",
+  //   // "john.doe@catchall.com",
+  //   // "no_at_symbol.com",
+  //   // "email@valid.com",
+  //   // "random.email123@invalid.",
+  //   // "test@example.net",
+  //   // "user@catchall.org",
+  //   // "invalid_email@.org",
+  //   // "12345@email.com",
+  //   // "catchall@randommail.xyz",
+  //   // "name@email.co.uk",
+  //   // "example@email",
+  //   // "invalid@.net",
+  //   // "valid.email@emailprovider.com",
+  //   // "user@catchallmail.com",
+  //   // "random@12345.email",
+  // ];
 
-  // email regex - ^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$
-  return res.json({
-    emails: emails.map((email) => {
-      return {
-        email,
-        isValid: isValidEmail(email),
-        isCatchAllEmail: isCatchAllEmail(email),
-      };
-    }),
-  });
+  applyPromiseToAllEmails(emails)
+    .then((resultMap) => {
+      console.log(JSON.stringify(resultMap));
+      return res.json({
+        emails: emails.map((email) => {
+          let smtpResponse;
+          // checking for error in the response
+          if (!resultMap[email][0]) {
+            smtpResponse = resultMap[email][1].response;
+          }
+          return {
+            email,
+            isValid: isValidEmail(email),
+            isCatchAllEmail: isCatchAllEmail(email),
+            smtpResponse: smtpResponse,
+          };
+        }),
+      });
+    })
+    .catch((error) => {
+      console.log("error");
+      res.json({
+        error,
+        emails: [],
+      });
+    });
 });
+
+const sendMailUsingNodemailer = async (DESTINATION_EMAIL) => {
+  // Simulating catch-all implementation
+  const incomingEmail = {
+    subject: "Test Email from Catch-All System",
+    body: "This is a test email from the catch-all system.",
+    // sender: "sender@example.com", // Replace with actual sender's email
+  };
+
+  return new Promise((resolve) => {
+    resolve(
+      sendEmail(incomingEmail.subject, incomingEmail.body, DESTINATION_EMAIL)
+    );
+  });
+};
+
+const applyPromiseToAllEmails = async (arr) => {
+  const promises = arr.map((element) => sendMailUsingNodemailer(element));
+  return Promise.all(promises).then((results) => {
+    // Map the results back to the original elements
+    const resultMap = {};
+    for (let i = 0; i < arr.length; i++) {
+      resultMap[arr[i]] = results[i];
+    }
+    return resultMap;
+  });
+};
 
 const isCatchAllEmail = (email) => {
   return false;
@@ -95,3 +139,4 @@ router.get("/health", (req, res, next) => {
 });
 
 module.exports = router;
+// message: 2.1.5 OK u21-20020a05622a011500b00419630a935esi3113757qtw.237 - gsmtp,
