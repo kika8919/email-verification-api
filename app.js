a0515604e0373b41a60b750832e67809ce038d7a
Kavita Gupta <124678732+kika8919@users.noreply.github.com>
2023-10-18 21:52:29 +0530

    Update app.js for cors

--- app.js
+++ app.js
@@ -2,12 +2,31 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4001;
 // const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (
+        [
+          "http://localhost:4200",
+          "https://email-verification-webapp.vercel.app",
+        ].indexOf(origin) !== -1 ||
+        !origin
+      ) {
+        callback(null, true);
+      } else {
+        console.log("cors err");
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
